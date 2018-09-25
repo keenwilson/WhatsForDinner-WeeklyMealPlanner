ecbc7f283f063df9737793f48a71839b8656d294
Tim Lukens <tblukens87@gmail.com>
2018-09-24 23:17:23 -0500

    Added recipeArr so Kysper can get information for database.

--- assets/javascript/edamam.js
+++ assets/javascript/edamam.js
@@ -1,9 +1,13 @@
-$(document).ready(function () {
+
+    var recipeArr = [];
+    $(document).ready(function () {
+
     app_id = "43a7d524";
     api_key = "0f1843d32c0f5990d058cf960ffee888";
 
     $("#search-recipe").on("click", function (e) {
         $("#display-recipe").empty();
+        recipeArr = [];
 
         console.log($("#find-recipe").val().trim())
         searchTerm = $("#find-recipe").val().trim();
@@ -17,7 +21,8 @@ $(document).ready(function () {
                 console.log(response)
                 response.hits.forEach(element => {
                     console.log(element)
-                    var calories = Math.floor(element.recipe.calories / element.recipe.yield);
+                    var recipe = element.recipe;
+                    var calories = Math.floor(recipe.calories / recipe.yield);
                     console.log(calories)
                     var addRecipe = '<div class="tile is-parent">'+
                     '<article class="tile is-child box">'+
@@ -26,14 +31,14 @@ $(document).ready(function () {
                          //   '<!-- Start Recipe Card -->''
                             '<div class="card-image">'+
                                 '<figure class="image is-square">'+
-                                    '<img src="'+element.recipe.image+'" alt="Placeholder image">'+
+                                    '<img src="'+recipe.image+'" alt="Placeholder image">'+
                                 '</figure>'+
                             '</div>'+
                             '<div class="card-content">'+
                                 '<div class="content">'+
-                                    '<h3 class="title">'+element.recipe.label+'</h3>'+
-                                    '<p class="calories">'+calories+' calories per serving.</p><p> Servings: '+element.recipe.yield+'</p>'+
-                                    '<p class="ingredients">'+element.recipe.ingredients.length+' Ingredients</p>'+
+                                    '<h3 class="title">'+recipe.label+'</h3>'+
+                                    '<p class="calories">'+calories+' calories per serving.</p><p> Servings: '+recipe.yield+'</p>'+
+                                    '<p class="ingredients">'+recipe.ingredients.length+' Ingredients</p>'+
                                     '<br>'+
                                     '<p class="show-modal subtitle"><a href="#">Get the Recipe</a></p>'+
                                 '</div>'+
@@ -48,9 +53,27 @@ $(document).ready(function () {
                     '</article>'+
                 '</div>';
                 $("#display-recipe").append(addRecipe);
+                var ingredientsArr = [];
+                recipe.ingredients.forEach(element => {
+                    // console.log(element.text)
+                    ingredientsArr.push(element.text)
+                });
+                var recipeDBinfo = {
+                    "image": recipe.image,
+                    "title": recipe.label,
+                    "caloriesPer": calories,
+                    "servings": recipe.yield,
+                    "numIngredients": recipe.ingredients.length,
+                    "ingredients": ingredientsArr
+                }
+                recipeArrFunc(recipeDBinfo);
                 });
             }
         });
         $("#find-recipe").val("");
     })
+    var recipeArrFunc = function(recipe) {
+        recipeArr.push(recipe);
+        console.log(recipeArr)
+    }
 });
