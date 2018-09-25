9bf5c1ebce5b4c121c1ba11e6a004d1e17be5f21
Tim L <tblukens87@gmail.com>
2018-09-24 19:58:50 -0500

    Basic ajax request started

---
+++ assets/javascript/edamam.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+    app_id = "43a7d524";
+    api_key = "0f1843d32c0f5990d058cf960ffee888";
+    
+    $.ajax({
+        type: "GET",
+        url: queryURL,
+        success: function (response) {
+            
+        }
+    });
+});
